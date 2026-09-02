abcd0138d0333c7f581e1f355b736e73730429e5
fredrikrudin <49065026+fredrikrudin@users.noreply.github.com>
2026-09-02 14:38:48 +0200

    Update bus_config.h

--- bus_config.h
+++ bus_config.h
@@ -1,1 +1,45 @@
+#ifndef BUS_CONFIG_H
+#define BUS_CONFIG_H
 
+#include <Arduino.h>
+#include <Preferences.h>
+
+struct BusSettings {
+    int rs485Baud;
+    int canSpeedKbps;
+};
+
+extern BusSettings busSettings;
+void initRS485(); 
+void initCAN();
+
+inline void loadBusSettings() {
+    Preferences prefs;
+    prefs.begin("bus_cfg", false);
+    busSettings.rs485Baud = prefs.getInt("baud", 9600);
+    busSettings.canSpeedKbps = prefs.getInt("can_speed", 500);
+    prefs.end();
+}
+
+inline void saveBusSettings() {
+    Preferences prefs;
+    prefs.begin("bus_cfg", false);
+    prefs.putInt("baud", busSettings.rs485Baud);
+    prefs.putInt("can_speed", busSettings.canSpeedKbps);
+    prefs.end();
+}
+
+inline void factoryResetBusSettings() {
+    Preferences prefs;
+    prefs.begin("bus_cfg", false);
+    prefs.clear(); 
+    prefs.end();
+    
+    busSettings.rs485Baud = 9600;
+    busSettings.canSpeedKbps = 500;
+    
+    initRS485();
+    initCAN();
+}
+
+#endif
